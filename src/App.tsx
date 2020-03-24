afdd9fdc2a723616917625f235983af5adec04b3
Tony Scialo <anthony@simplifeye.co>
2020-03-23 21:34:00 -0400

    added react router to project

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,24 @@ import { createStore, applyMiddleware } from 'redux'
 import AppReducer from './reducers/app/app.reducer'
 import thunkMiddleware from 'redux-thunk'
 import { Provider } from 'react-redux'
+import Login from './login/login'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const store = createStore(AppReducer, applyMiddleware(thunkMiddleware))
 
 function App() {
   return (
     <Provider store={store}>
-      <div>TEST</div>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </Provider>
   )
 }
---
+++ src/login/login.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Login = () => {
+  return <div>THIS IS THE LOGIN</div>
+}
+
+export default Login
