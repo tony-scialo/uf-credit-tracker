6d279be67add695b6bca9ddd493b18255f5c30bd
Tony Scialo <anthony@simplifeye.co>
2020-03-24 21:32:45 -0400

    started laying out left side of login box

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../reducers/reducer.state'
 import './login.scss'
+import { Button, TextField, InputAdornment } from '@material-ui/core'
+import LockOpenIcon from '@material-ui/icons/LockOpen'
+import PersonIcon from '@material-ui/icons/Person'
 
 const Login = () => {
   const username = useSelector((state: RootState) => state.app.username)
@@ -9,8 +12,36 @@ const Login = () => {
   return (
     <div className="login">
       <div className="login-box">
-        <div>{username}</div>
-        <div>{password}</div>
+        <div className="left">
+          <form noValidate autoComplete="off">
+            <TextField
+              className="input"
+              id="username-field"
+              label="Username"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <PersonIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              className="input"
+              id="password-field"
+              label="Password"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <LockOpenIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </form>
+          <Button variant="contained">Sign In</Button>
+        </div>
+        <div className="right"></div>
       </div>
     </div>
   )
