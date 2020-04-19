e09dd4b890caf68777ce85ae0870948a59442111
Tony Scialo <anthony@simplifeye.co>
2020-04-19 13:56:43 -0400

    added rechart to app

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -7,6 +7,16 @@ import { getDashboardData } from '../reducers/dashboard/dashboard.actions'
 import ErrorMessage from '../error-message/ErrorMessage'
 import { CircularProgress } from '@material-ui/core'
 
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from 'recharts'
+
 const Dashboard = () => {
   const dashboard = useSelector((state: RootState) => state.dashboard)
   const dispatch = useDispatch()
@@ -51,11 +61,70 @@ const Dashboard = () => {
       </div>
     )
   } else {
+    const data = [
+      {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+      },
+      {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+      },
+      {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+      },
+      {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+      },
+      {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+      },
+      {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+      },
+      {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+      },
+    ]
     return (
       <div className="dashboard">
         <Header />
         <div className="main-body">
-          <div className="main-body-inner">DATA</div>
+          <div className="main-body-inner">
+            <LineChart
+              width={730}
+              height={250}
+              data={data}
+              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+          </div>
         </div>
       </div>
     )
