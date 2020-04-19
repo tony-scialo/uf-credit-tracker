1e33e0a3af5f77ce647d1ea26c6fff41dab2bc97
Tony Scialo <anthony@simplifeye.co>
2020-04-18 21:36:12 -0400

    header and body styled

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -6,7 +6,9 @@ const Dashboard = () => {
   return (
     <div className="dashboard">
       <Header />
-      <div className="main-body">Body</div>
+      <div className="main-body">
+        <div className="main-body-inner">Body</div>
+      </div>
     </div>
   )
 }
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -5,12 +5,14 @@ import './Header.scss'
 const Header = () => {
   return (
     <div className="header">
-      <div className="left">
-        <CreditCardIcon />
-        <div className="title">Credit Tracker +</div>
-      </div>
-      <div className="right">
-        <div className="username">tscialo@uf.org</div>
+      <div className="header-inner">
+        <div className="left">
+          <CreditCardIcon />
+          <div className="title">Credit Tracker +</div>
+        </div>
+        <div className="right">
+          <div className="username">tscialo@uf.org</div>
+        </div>
       </div>
     </div>
   )
