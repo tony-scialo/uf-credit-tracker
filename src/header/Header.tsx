ee34fd06202d80401611d18caa0ef23091f86b00
Tony Scialo <anthony@simplifeye.co>
2020-04-19 21:55:37 -0400

    menu added

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -3,18 +3,37 @@ import CreditCardIcon from '@material-ui/icons/CreditCard'
 import './Header.scss'
 import { useSelector } from 'react-redux'
 import { RootState } from '../reducers/reducer.state'
+import { useHistory } from 'react-router'
 
 const Header = () => {
   const username = useSelector((state: RootState) => state.app.username)
+  const history = useHistory()
+
+  const navigate = (path: string) => {
+    history.push(path)
+  }
+
   return (
-    <div className="header">
-      <div className="header-inner">
-        <div className="left">
-          <CreditCardIcon />
-          <div className="title">Credit Tracker +</div>
+    <div className="header-wrapper">
+      <div className="header">
+        <div className="header-inner inner">
+          <div className="left">
+            <CreditCardIcon />
+            <div className="title">Credit Tracker +</div>
+          </div>
+          <div className="right">
+            <div className="username">{username ? username : '-'}</div>
+          </div>
         </div>
-        <div className="right">
-          <div className="username">{username ? username : '-'}</div>
+      </div>
+      <div className="menu-wrapper">
+        <div className="menu-inner inner">
+          <div className="menu-item" onClick={() => navigate('/dashboard')}>
+            Dashboard
+          </div>
+          <div className="menu-item" onClick={() => navigate('/totals')}>
+            Total Records
+          </div>
         </div>
       </div>
     </div>
