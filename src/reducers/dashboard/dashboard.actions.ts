d1a5159b8a7a4519226a5743a828d46b6a2be264
Tony Scialo <anthony@simplifeye.co>
2020-04-21 13:52:29 -0400

    added fake data for payments call

--- src/reducers/dashboard/dashboard.actions.ts
+++ src/reducers/dashboard/dashboard.actions.ts
@@ -102,7 +102,13 @@ const getTopRegions = () => {
   return Promise.resolve({
     status: 'success',
     data: {
-      topRegions: [{ regionNo: 1, regionName: '', totalPayments: 4444 }],
+      topRegions: [
+        { regionNo: 9, regionName: 'Africa', totalPayments: 1815167 },
+        { regionNo: 4, regionName: 'Western Europe', totalPayments: 1805543 },
+        { regionNo: 3, regionName: 'Scandanavia', totalPayments: 1686398 },
+        { regionNo: 7, regionName: 'China', totalPayments: 1678806 },
+        { regionNo: 5, regionName: 'Easten Europe', totalPayments: 1678345 },
+      ],
     },
     error: '',
   })
@@ -112,7 +118,13 @@ const getTopCorp = () => {
   return Promise.resolve({
     status: 'success',
     data: {
-      topCorp: [{ corpNo: 1, corpName: '', totalPayments: 4444 }],
+      topCorp: [
+        { corpNo: 94, corpName: 'E*Trade', totalPayments: 79078 },
+        { corpNo: 42, corpName: 'Mastercard Inc.', totalPayments: 69676 },
+        { corpNo: 34, corpName: 'A.O. Smith Corp', totalPayments: 67211 },
+        { corpNo: 71, corpName: 'Align Technology', totalPayments: 67073 },
+        { corpNo: 13, corpName: 'CVS Health', totalPayments: 64746 },
+      ],
     },
     error: '',
   })
@@ -123,7 +135,36 @@ const getTopMembers = () => {
     status: 'success',
     data: {
       topMember: [
-        { memberNo: 1, memberFName: '', memberLName: '', totalPayments: 4444 },
+        {
+          memberNo: 1,
+          memberFName: 'Diego',
+          memberLName: 'Abreba',
+          totalPayments: 4444,
+        },
+        {
+          memberNo: 2,
+          memberFName: 'Brenda',
+          memberLName: 'Smith',
+          totalPayments: 4444,
+        },
+        {
+          memberNo: 3,
+          memberFName: 'John',
+          memberLName: 'Yank',
+          totalPayments: 4444,
+        },
+        {
+          memberNo: 4,
+          memberFName: 'Tony',
+          memberLName: 'Gordon',
+          totalPayments: 4444,
+        },
+        {
+          memberNo: 5,
+          memberFName: 'Kelsey',
+          memberLName: 'Scezer',
+          totalPayments: 4444,
+        },
       ],
     },
     error: '',
