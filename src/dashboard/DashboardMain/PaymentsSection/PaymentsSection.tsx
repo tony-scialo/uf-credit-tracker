76c8496c71b10511c2b6a30b462b55439e7bd231
Tony Scialo <anthony@simplifeye.co>
2020-04-20 20:21:46 -0400

    removed strict mode from app

--- src/dashboard/DashboardMain/PaymentsSection/PaymentsSection.tsx
+++ src/dashboard/DashboardMain/PaymentsSection/PaymentsSection.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { FormControl, InputLabel, Select, MenuItem } from '@material-ui/core'
 
 const PaymentsSection = () => {
+  const [days, setDays] = useState(30)
+
+  const handleChange = (event: any) => {
+    setDays(event.target.value)
+    console.log(event.target.value)
+  }
+
+  const wrapper: any = useRef(null)
+
   return (
     <div className="payments-section">
-      <div>Payments Baby</div>
+      <div className="filter">
+        <FormControl ref={wrapper}>
+          <InputLabel id="paymentInputLabel">Previous</InputLabel>
+          <Select
+            labelId="paymentInputLabel"
+            id="paymentInput"
+            value={days}
+            onChange={handleChange}
+          >
+            <MenuItem value={30}>30 days</MenuItem>
+            <MenuItem value={60}>60 days</MenuItem>
+            <MenuItem value={90}>90 days</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className="data"></div>
     </div>
   )
 }
